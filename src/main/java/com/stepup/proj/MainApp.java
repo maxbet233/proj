8f88aacdc65559e37c09d4b71d0cd9836b57901e
maxbet233 <maks_233@list.ru>
2024-11-07 22:49:31 +0300

    добавлен новый класс

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,7 +3,8 @@ package com.stepup.proj;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("Hello2!");
+        System.out.println("Hello0!");
+        System.out.println("Hello3222!");
         System.out.println("Hello3!");
     }
 }
