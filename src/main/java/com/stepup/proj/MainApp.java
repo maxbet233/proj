d6e15c6e333e6295281223b95e3793d767efc834
maxbet233 <maks_233@list.ru>
2024-11-07 22:35:48 +0300

    edit print

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello2!");
+        System.out.println("Hello3!");
     }
 }
